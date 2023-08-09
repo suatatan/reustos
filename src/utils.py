38063c4e005767c16fcc65618e62e273e718737b
atanvectorsolv <atan@vectorsolv.com>
2023-08-09 17:52:59 +0300

    ed

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
-# %% 
-import pandas as pd
-df = pd.read_excel(r"C:\Users\suat.atan\Downloads\walmart_page_list.xlsx")
-# %%
-def get_product_id_from_walmart_link(url):
-    parts = url.split('/')
-    last_part = parts[-1]
-    return last_part
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.
+
+# duplicated entry remover in data frame 
+
+def remove_duplicate(df):
+    df = df.drop_duplicates(subset=['title'], keep='first')
+    return df
+
+# function to remove the stop words
+def remove_stopwords(text):
+    stop_words = set(stopwords.words('english'))
---
+++ src/walmart/utils.py
@@ -0,0 +1,3 @@
+# search_keyword_and_get_links
+# get_product_name_by_url
+# get_product_code_by_url
